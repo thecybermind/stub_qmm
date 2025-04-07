f6a9be8eacc9ee4240a4420ec250c6f9121e9909
Kevin Masterson <cybermind@gmail.com>
2025-04-06 23:14:20 -0400

    update version and add .rc file to project

--- include/version.h
+++ include/version.h
@@ -12,9 +12,33 @@ Created By:
 #ifndef __STUB_QMM_VERSION_H__
 #define __STUB_QMM_VERSION_H__
 
-#define STUB_QMM_VERSION			"2.0.0"
-#define STUB_QMM_VERSION_DWORD		0,2,0,0
-#define STUB_QMM_COMPILE			__TIME__ " " __DATE__
-#define STUB_QMM_BUILDER			"cybermind"
+#define STRINGIFY(x) STRINGIFY2(x)
+#define STRINGIFY2(x) #x
+
+#define STUB_QMM_VERSION_MAJOR	2
+#define STUB_QMM_VERSION_MINOR	0
+#define STUB_QMM_VERSION_REV	0
+
+#define STUB_QMM_VERSION		STRINGIFY(STUB_QMM_VERSION_MAJOR) "." STRINGIFY(STUB_QMM_VERSION_MINOR) "." STRINGIFY(STUB_QMM_VERSION_REV)
+
+#if defined(_WIN32)
+#define STUB_QMM_OS             "Windows"
+#ifdef _WIN64
+#define STUB_QMM_ARCH           "x86_64"
+#else
+#define STUB_QMM_ARCH           "x86"
+#endif
+#elif defined(__linux__)
+#define STUB_QMM_OS             "Linux"
+#ifdef __LP64__
+#define STUB_QMM_ARCH           "x86_64"
+#else
+#define STUB_QMM_ARCH           "x86"
+#endif
+#endif
+
+#define STUB_QMM_VERSION_DWORD	STUB_QMM_VERSION_MAJOR , STUB_QMM_VERSION_MINOR , STUB_QMM_VERSION_REV , 0
+#define STUB_QMM_COMPILE		__TIME__ " " __DATE__
+#define STUB_QMM_BUILDER		"cybermind"
 
 #endif // __STUB_QMM_VERSION_H__
