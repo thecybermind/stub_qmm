4433d6791df332d35a661b4e69a8c33b7a14983e
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 17:23:21 +0200

    add SOF2SP build

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,4 +1,4 @@
-for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP STEF2 STVOYHM STVOYSP WET) do (
+for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x86
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x64
 )
