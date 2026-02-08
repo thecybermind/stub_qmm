c1daf48cc4156aeaa88c1648d11d1c790c0dde44
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 19:45:59 +0200

    update windows build scripts to exit if %errorlevel% is not 0

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,4 +1,6 @@
-for %%x in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
-    msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x86
-    msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%x /p:Platform=x64
+for %%G in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
+    msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x86
+	if not ERRORLEVEL 0 exit /b
+    msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x64
+	if not ERRORLEVEL 0 exit /b
 )
