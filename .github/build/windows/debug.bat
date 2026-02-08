13523b05105f0852ae7f66112e5085a5ebd4e007
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 21:40:13 +0200

    testing this for batch

--- .github/build/windows/debug.bat
+++ .github/build/windows/debug.bat
@@ -1,6 +1,12 @@
 for %%G in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Debug-%%G /p:Platform=x86
-	if not ERRORLEVEL 0 exit /b 1
+	IF %ERRORLEVEL% NEQ 0 (
+	exit /b 1
+	goto :eof
+	)
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Debug-%%G /p:Platform=x64
-	if not ERRORLEVEL 0 exit /b 1
+	IF %ERRORLEVEL% NEQ 0 (
+	exit /b 1
+	goto :eof
+	)
 )
