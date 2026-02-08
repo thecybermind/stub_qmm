be3614cbddfb5ccf6709dede83d9a2e9c03bff63
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 21:49:14 +0200

    do not run Q2R x86 build

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,5 +1,5 @@
 for %%G in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
-    msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x86
+    if [%%G] NEQ [Q2R] msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x86
 	if errorlevel 1 exit /b errorlevel
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x64
 	if errorlevel 1 exit /b errorlevel
