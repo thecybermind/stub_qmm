bcaa49dd2b922fadadffc5b6e184370b47e557b2
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 21:43:06 +0200

    another batch fix attempt

--- .github/build/windows/release.bat
+++ .github/build/windows/release.bat
@@ -1,12 +1,6 @@
 for %%G in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x86
-	IF %ERRORLEVEL% NEQ 0 (
-	exit /b 1
-	goto :eof
-	)
+	if errorlevel 1 exit /b errorlevel
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Release-%%G /p:Platform=x64
-	IF %ERRORLEVEL% NEQ 0 (
-	exit /b 1
-	goto :eof
-	)
+	if errorlevel 1 exit /b errorlevel
 )
