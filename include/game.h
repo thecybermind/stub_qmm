4dd4e4140f8e7df31cf51d677bd875dffb69cb37
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 19:27:10 +0200

    added sof2sp support

--- include/game.h
+++ include/game.h
@@ -120,6 +120,11 @@ Created By:
     #include <sof2mp/game/g_local.h>
     #include <game_sof2mp.h>
     #define GAME_STR "SOF2MP"
+#elif defined(GAME_SOF2SP)
+    // #include <sof2mp/game/g_local.h>
+    #include <game_sof2sp.h>
+    #define GAME_CLIENT_USERINFO_CHANGED -9999 // just needs to be a msg number that won't exist so it won't compare true
+    #define GAME_STR "SOF2SP"
 #elif defined(GAME_STEF2)
     #include <stef2/game/g_local.h>
     #include <stef2/game/bg_public.h>
