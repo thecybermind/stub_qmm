40729ae3a8be883d50797aa72ccc2213c8d2798c
Kevin Masterson <k.m.masterson@gmail.com>
2026-02-08 21:10:29 +0200

    fix

--- .github/build/windows/debug.bat
+++ .github/build/windows/debug.bat
@@ -1,6 +1,6 @@
 for %%G in (COD11MP CODMP CODUOMP JAMP JASP JK2MP JK2SP MOHAA MOHBT MOHSH Q2R Q3A QUAKE2 RTCWMP RTCWSP SIN SOF2MP SOF2SP STEF2 STVOYHM STVOYSP WET) do (
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Debug-%%G /p:Platform=x86
-	if not ERRORLEVEL 0 exit /b
+	if not ERRORLEVEL 0 exit /b 1
     msbuild .\msvc\stub_qmm.vcxproj /p:Configuration=Debug-%%G /p:Platform=x64
-	if not ERRORLEVEL 0 exit /b
+	if not ERRORLEVEL 0 exit /b 1
 )
